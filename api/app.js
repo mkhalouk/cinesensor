fbdcdfabbee9f2266a0d784180193eb5fbd92124
mkhalouk <musta.khalouk@gmail.com>
2023-06-21 00:24:39 +0200

    WIP: working on that shitty api + mqtt

--- api/app.js
+++ api/app.js
@@ -1,17 +1,27 @@
+require('dotenv').config();
 const express = require('express');
+const { connectToDb } = require('./service/database');
 
 const app = express();
 const PORT = 5000;
 
-app.listen(PORT, (error) =>{
-	if(!error)
-		console.log("API is Successfully Running, and App is listening on port "+ PORT)
-	else
+app.listen(PORT, (error) => {
+	if (!error) {
+		// Connexion à la base de données
+		(async () => { await connectToDb(); })();
+		console.log("API is Successfully Running, and App is listening on port " + PORT)
+
+		// set timeout : callback func bash tgeneri numbers u func katsavi f basedonnes u func katpublish via mqtt data
+		// TODO: api sends to back to stock in the database
+		// humidity, noise, pressure, temperature
+	}
+	else {
 		console.log("Error occurred, api can't start", error);
 	}
+}
 );
 
 
 app.get('/', (req, res) => {
-    res.send('Hello world! This is my API.');
+	res.send('Hello world! This is my API.');
 });
